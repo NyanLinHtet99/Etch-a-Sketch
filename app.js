531a080c386316f020c080903b1b38c279608112
Nyan Lin Htet <xylonic3@gmail.com>
2022-07-22 15:47:58 +0630

    Add color generator and drawing effect

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-let squareNo = 0;
-do squareNo = Math.round(parseInt(prompt('Enter the number of squares in a row')));
-while(typeof(squareNo)!= 'number');
-let size = squareNo *squareNo;
+
 const body = document.querySelector('body');
-createCanvas();
+const button = document.querySelector('button');
+
+button.addEventListener('click',createCanvas);
 function createCanvas(){
+    let temp = document.getElementById('container-id');
+    if(typeof(temp)!= 'undefined'&& temp !=null){
+        body.removeChild(temp);
+    }
+    const value = document.querySelector('input').value;    
+    let squareNo = Math.round(parseInt(value));
+    let size = squareNo *squareNo;
     const container = document.createElement('div');
+    container.id = 'container-id';
     container.style.gridTemplateColumns = `repeat(${squareNo},1fr)`;
     container.style.gridTemplateRows = `repeat(${squareNo},1fr)`;
 
@@ -14,7 +21,20 @@ function createCanvas(){
         let div = document.createElement('div');
         div.classList.add('square');
         container.appendChild(div);
+        div.addEventListener('mouseover',(e)=>{
+           e.target.style.backgroundColor = colorGenerate();
+        })
     }
     body.appendChild(container);
+}
 
+function draw(e){
+    
+}
+function colorGenerate(){
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+    let a = Math.random() * 0.3 + 0.7;
+    return `rgba(${r},${g},${b},${a})`
 }
